fb7897b29a4db0774295598c1d0f7066422a5601
Jarod Kurland <jarodsmk@gmail.com>
2022-06-10 09:52:24 +0200

    chore: added dynamsoft code that breaks test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import Dynamsoft from 'dwt';
+import { WebTwain } from 'dwt/dist/types/WebTwain';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  thisBreaks: WebTwain = Dynamsoft.DWT.GetWebTwain('test');
   title = 'dwt-angular-jest';
 }
